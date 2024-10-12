498d9a7759461e3c2ead128103ec54142b0adce6
prapooskur <61389919+prapooskur@users.noreply.github.com>
2024-10-12 06:31:31 -0700

    made button fill up full width

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -9,7 +9,7 @@ export const FilterButton = ({label, onClick}: FilterButtonProps) => {    // Rou
     // filter bcycle stops by on/off campus, stations with available bikes, 
     return (
         <div style={{ width: 150, margin: '5px 0px 5px 0px', fontSize: 15, borderRadius: 5, backgroundColor: 'gray' }}>
-            <button onClick={onClick}>{label}</button>
+            <button onClick={onClick} style={{ width: '100%' }}>{label}</button>
         </div>
     )
 } 
