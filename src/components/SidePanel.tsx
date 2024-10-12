d6792f1ee0e805834086c244087b5a97f46a026f
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2024-10-12 14:05:30 -0700

    sidebar

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,51 +1,208 @@
-import { useState } from 'react';
-import { Dialog, DialogBackdrop, DialogPanel, DialogTitle, TransitionChild } from '@headlessui/react';
+import { useState } from "react";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+} from "@headlessui/react";
 // import { XMarkIcon } from '@heroicons/react/24/outline'
-import './SidePanel.css';
+import "./SidePanel.css";
+
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 interface SidePanelProps {
   stationID: string;
 }
 
+const drawerWidth = 440;
+
 export const SidePanel = ({ stationID }: SidePanelProps) => {
   const [open, setOpen] = useState(true);
 
-  return (
-    <Dialog open={open} onClose={() => setOpen(false)} className="side-panel-dialog">
-      <DialogBackdrop
-        transition
-        className="side-panel-backdrop"
-      />
+  const Main = styled("main", {
+    shouldForwardProp: (prop) => prop !== "open",
+  })<{
+    open?: boolean;
+  }>(({ theme }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    variants: [
+      {
+        props: ({ open }) => open,
+        style: {
+          transition: theme.transitions.create("margin", {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+          marginLeft: 0,
+        },
+      },
+    ],
+  }));
 
-      <div className="side-panel-fixed-container">
-        <div className="side-panel-absolute-container">
-          <div className="side-panel-inner-container">
-            <DialogPanel
-              transition
-              className="side-panel-dialog-panel"
-            >
-              <TransitionChild>
-                <div className="side-panel-close-button-container">
-                  <button
-                    type="button"
-                    onClick={() => setOpen(false)}
-                    className="side-panel-close-button"
-                  >
-                    <span className="side-panel-invisible-span" />
-                    <span className="sr-only">Close panel</span>
-                  </button>
-                </div>
-              </TransitionChild>
-              <div className="side-panel-content">
-                <div className="side-panel-title">
-                  <DialogTitle>Panel title</DialogTitle>
-                </div>
-                <div className="side-panel-body">{/* Your content here */}</div>
-              </div>
-            </DialogPanel>
-          </div>
-        </div>
-      </div>
-    </Dialog>
+  const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  }));
+
+  const theme = useTheme();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    // <Dialog open={open} onClose={() => setOpen(false)} className="side-panel-dialog">
+    //   {/* <DialogBackdrop
+    //     transition
+    //     className="side-panel-backdrop"
+    //   /> */}
+
+    //   <div className="side-panel-fixed-container">
+    //     <div className="side-panel-absolute-container">
+    //       <div className="side-panel-inner-container">
+    //         <DialogPanel
+    //           transition
+    //           className="side-panel-dialog-panel"
+    //         >
+    //           <TransitionChild>
+    //             <div className="side-panel-close-button-container">
+    //               <button
+    //                 type="button"
+    //                 onClick={() => setOpen(false)}
+    //                 className="side-panel-close-button"
+    //               >
+    //                 <span className="side-panel-invisible-span" />
+    //                 <span className="sr-only">Close panel</span>
+    //               </button>
+    //             </div>
+    //           </TransitionChild>
+    //           <div className="side-panel-content">
+    //             <div className="side-panel-title">
+    //               <DialogTitle>Panel title</DialogTitle>
+    //             </div>
+    //             <div className="side-panel-body">{/* Your content here */}</div>
+    //           </div>
+    //         </DialogPanel>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </Dialog>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["All mail", "Trash", "Spam"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Main open={open}>
+        <DrawerHeader />
+        <Typography sx={{ marginBottom: 2 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+          dolor purus non enim praesent elementum facilisis leo vel. Risus at
+          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
+          quisque non tellus. Convallis convallis tellus id interdum velit
+          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
+          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
+          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
+          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
+          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
+          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
+          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
+          faucibus et molestie ac.
+        </Typography>
+        <Typography sx={{ marginBottom: 2 }}>
+          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
+          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
+          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
+          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
+          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
+          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
+          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
+          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
+          morbi tristique senectus et. Adipiscing elit duis tristique
+          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+          posuere sollicitudin aliquam ultrices sagittis orci a.
+        </Typography>
+      </Main>
+    </Box>
   );
 };
