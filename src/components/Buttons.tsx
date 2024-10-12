96e24a78e6dd4f64cc4c15c0fa4096f8f8b6dc7e
Ch4xm <jaytripathi100@gmail.com>
2024-10-12 07:05:20 -0700

    add filter styling

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -8,8 +8,8 @@ interface FilterButtonProps {
 export const FilterButton = ({label, onClick}: FilterButtonProps) => {    // Rounded rectangle buttons on right side of page to 
     // filter bcycle stops by on/off campus, stations with available bikes, 
     return (
-        <div style={{ width: 150, margin: '5px 0px 5px 0px', fontSize: 15, borderRadius: 5, backgroundColor: 'gray' }}>
-            <button onClick={onClick} style={{ width: '100%' }}>{label}</button>
+        <div style={{ width: 175, margin: '7.5px 0px 7.5px 0px', fontSize: 18 }}>
+            <button onClick={onClick} style={{  borderRadius: 50, padding: 17.5, width: '100%', backgroundColor: 'black' }}>{label}</button>
         </div>
     )
 } 
--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -6,6 +6,7 @@ interface FilterButtonProps {
 
 export const Filter = ({ label }: FilterButtonProps) => {
     return (
+
         <div style={{
             position: 'fixed',
             top: '12.5%',
@@ -16,13 +17,23 @@ export const Filter = ({ label }: FilterButtonProps) => {
             // justifyContent: 'center',
             backgroundColor: 'gray',
             width: '12.5vw',
-            padding: 0,
+            padding: 10,
             borderRadius: '10px',
             alignItems: 'center',
             // color: 'white',
-            // opacity: 0.8,
-            // fontWeight: 'bold'
+            // filter: 'blur(1px)',
+            opacity: 0.85,
+            fontWeight: 'bold',
+            fontSize: '1.4vw',
+            // boxShadow: '0px 0px 5px .1px black',
+
         }}>
+            <div style={{
+                filter: 'blur(5px)',
+                width: '100%',
+                height: '100%',
+                backgroundColor: '',
+            }} />
             <label style={{
                 backgroundColor: 'gray',
                 padding: '5px',
