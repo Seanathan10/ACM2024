d5bd3d4eb8f9733486d5b61fc9e9840df7b9813e
Sean Singh <38306929+Seanathan10@users.noreply.github.com>
2024-10-12 05:25:54 -0700

    cleanup code

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,6 @@ import MapComponent from "./Map";
 
 import { useEffect, useState } from "react";
 import { getBcycleInformationJSON, getBcycleStatusJSON } from "./api/BCycle";
-// import { getBcycleInformationJSON, getBcycleStatusJSON } from "api/BCycle";
-
-// export function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en">
-//       <head>
-//         <meta charSet="utf-8" />
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//       </head>
-//       <body>
-//         <Map
-//           height={1500}
-//           defaultCenter={[36.995544, -122.06037]}
-//           defaultZoom={15}
-//         >
-//           <Marker width={50} anchor={[50.879, 4.6997]} />
-//         </Map>
-//         <Scripts />
-//       </body>
-//     </html>
-//   );
-// }
 
 export default function App() {
   const [statusData, setStatusData] = useState({});
@@ -44,7 +22,6 @@ export default function App() {
   return (
     <>
       Hello
-      {/* <Outlet />; */}
       <MapComponent></MapComponent>
     </>
   );
